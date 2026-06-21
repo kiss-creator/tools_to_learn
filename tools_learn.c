961985506f3b091ffb33add8823102bf90b6fe52
kiss-creator <tripoldought@gmail.com>
2026-06-21 20:40:30 +0600

    Create tools_learn.c

---
+++ tools_learn.c
@@ -0,0 +1,28 @@
+Tools to learn : 
+1. Vim
+2. git and git_hub
+3. C 
+
+(((Programming and Math :))))
+
+The logic(native code) 
+1.
+2.
+3.
+...................
+
+then 
+
+C implementation
+
+#include<stdio.h>
+int main(void) {
+	printf("Hello Dibbo") ;
+	return 0 ; 	
+}
+
+How it work -> :
+1. 
+2. 
+3.
+..................
